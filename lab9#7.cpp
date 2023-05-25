e07efea251024b74b4e7418c65f5577558ed4b21
benjaminzhong1 <97631450+benjaminzhong1@users.noreply.github.com>
2023-05-25 16:26:22 -0400

    Create lab9#7.cpp
    lab 9 part 7
    
    - making a countdown 5x5 table starting from 10

---
+++ lab9#7.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+  {
+  for(int r=5;r>=1;r--)
+    {
+      for(int c=5;c>=1;c--)
+        {
+          cout<<c+r<<"\t";
+        }
+    cout<<endl;
+    }
+  return 0;
+  }
